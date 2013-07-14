5aa8d74a297a9a353ea37155ebe548f13b7600a4
Michal Kawalec <michal@bazzle.me>
2013-07-14 02:53:16 +0200

    API is on api dir

--- api/__init__.py
+++ api/__init__.py
@@ -24,11 +24,11 @@ import json
 import smtplib
 import pickle
 
-@app.route('/')
+@app.route('/api')
 def home():
     return '42'
 
-@app.route('/confirm/<code_id>', methods=['POST'])
+@app.route('api/confirm/<code_id>', methods=['POST'])
 @crossdomain(origin='*')
 def confirm(code_id):
     try:
@@ -130,7 +130,7 @@ class SignaturesView(FlaskView):
         sig = Signature('test', 'test', 'test')
         return jsonify(data=class_spec(sig))
     
-SignaturesView.register(app)
+SignaturesView.register(app, route_base='api/signatures/')
 
 def send_email(signature):
     msg = u'Witaj %(name)s,\n'\
