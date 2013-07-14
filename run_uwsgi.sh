503b4882a50a084fa175abd939ecfe9b0bf39701
Michal Kawalec <michal@bazzle.me>
2013-07-14 02:32:56 +0200

    Removed venv invocation

--- run_uwsgi.sh
+++ run_uwsgi.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-source /var/www/prism/.venv/bin/activate
 cd /var/www/prism && uwsgi -s /dev/shm/prism.sock --module api --callable app -p 8 --stats :8252 --reload-on-as 150 --reload-on-exception --max-requests 10000 --uid $1 --pidfile2 /tmp/prism.pid
